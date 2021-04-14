b1f06b3a85764d782eca51d3386952c5cbef680d
Karen Roh <karenroh@hotmail.com>
2021-04-14 10:38:18 -0400

    Successfully writes to file and generates readme with all info

---
+++ Develop/README.md
@@ -0,0 +1,24 @@
+# README Generator
+## Description
+This is a project that generates a professional readme file.
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](tests)
+* [Questions](#questions)
+## Installation
+To install necessary dependencies, run the following command:  
+npm i
+## Usage  
+Watch my walkthrough video!!!!!!!!
+## License    
+This project is licensed under the GPL 3.0 license.
+## Contributing  
+Idk do whatever you want with it
+## Tests  
+To run tests, run the following command:  
+npm test
+## Questions  
+If you have any questions about the repo, open an issue or contact me directly at karenroh@hotmail.com. You can find more of my work at kroh1031.
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,101 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your project's name?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please write a short description of your project",
+    name: "description",
+  },
+  {
+    type: "list",
+    message: "What kind of license should your project have?",
+    name: "license",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3"],
+  },
+  {
+    type: "input",
+    message: "What command should be run to install dependencies?",
+    name: "installation",
+    default: "npm i",
+  },
+  {
+    type: "input",
+    message: "What command should be run to run tests?",
+    name: "tests",
+    default: "npm test",
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about using the repo?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about contributing to the repo?",
+    name: "contributing",
+  },
+];
+inquirer
+  .prompt(questions)
+  .then((data) => {
+    console.log(data);
+    console.log(data.title);
+    console.log(data.description);
+    console.log(data.installation);
+    console.log(data.usage);
+    console.log(data.license);
+    console.log(data.contributing);
+    console.log(data.tests);
+    console.log(data.email);
+    console.log(data.username);
+    fs.writeFile("README.md", generateMarkdown(data), (err) =>
+      err ? console.log(err) : console.log("Success!!!")
+    );
+  })
+  .catch((err) => console.log(err));
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+//Order on readme
+//Title
+//License badge
+//Description
+//Table of Contents
+//Installation (To install necessary dependencies, run the following command: npm i)
+//Usage
+//License (This project is licensed under the MIT license.)
+//Contributing
+//Tests (To run tests, run the following command: npm test)
+//Questions (If you have any questions about the repo, open an issue or contact me directly at karenroh@hotmail.com. You can find more of my work at kroh1031.)
+
+//Steps to take:
+//1. Prompt user with questions
+//2. Save data to file
+//3. Generate readme
