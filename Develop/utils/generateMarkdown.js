7ba558084d70c605b2bfec7689a80ffa9ed214a2
Karen Roh <karenroh@hotmail.com>
2021-04-14 18:13:52 -0400

    license links render successfully

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  console.log(license);
   if (license === "MIT") {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
   } else if (license === "APACHE 2.0") {
@@ -17,7 +16,19 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return `https://opensource.org/licenses/MIT`;
+  } else if (license === "APACHE 2.0") {
+    return `https://opensource.org/licenses/Apache-2.0`;
+  } else if (license === "GPL 3.0") {
+    return `https://img.shields.io/badge/License-GPLv3-blue.svg`;
+  } else if (license === "BSD 3") {
+    return `https://img.shields.io/badge/License-BSD%203--Clause-blue.svg`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -27,6 +38,7 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}  
   ${renderLicenseBadge(data.license)}  
+   
 ## Description
 ${data.description}
 ## Table of Contents 
@@ -43,6 +55,7 @@ ${data.installation}
 ${data.usage}
 ## License    
 This project is licensed under the ${data.license} license.
+${renderLicenseLink(data.license)}
 ## Contributing  
 ${data.contributing}
 ## Tests  
